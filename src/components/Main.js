e1e623758b3ec63179707e8e1be75422bb4959d9
Joe Keohah <jkeohan@gmail.com>
2017-10-03 19:04:46 -0400

    finished design for smallPlanets in main - 1hr

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
+import saturnSmall from './saturnSmall.jpeg';
+import allPlanetsSmall from './allPlanetsSmall.jpeg';
+
+const divStyle = {
+  backgroundImage: 'url(' + allPlanetsSmall + ')',
+};
 
 const Main = () => (
-  <div class="container">
-  <row>
-    <col-4>
-      <h1>Planets</h1>
-      <p>Info</p>
-    </col-4>
-    <col-4>
-      <h1>Planets</h1>
-      <p>Info</p>
-    </col-4>
-    <col-4>
-      <h1>Planets</h1>
-      <p>Info</p>
-    </col-4>
-  </row>
-</div>
-
+  <main>
+    <Grid>
+      <Row className='show-grid' id="allPlanets">
+        <Col sm={4}>
+          <span style={divStyle} className="allPlanetsSmall"></span>
+          <h1>Our Planets</h1>
+          <hr />
+          <p>Learn more about the amazing planets in our Solar System</p>
+        </Col>
+        <Col sm={4}>
+          <h1>The Milky Way</h1>
+          <p>Info</p>
+        </Col>
+        <Col sm={4}>
+          <h1>The Universe</h1>
+          <p>Info</p>
+        </Col>
+      </Row>
+    </Grid>
+  </main>
 )
 
 export default Main;
